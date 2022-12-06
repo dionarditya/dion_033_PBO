1e6159d27036951ca56d56398173be0cde9a3980
dionarditya <114129205+dionarditya@users.noreply.github.com>
2022-12-06 12:51:05 +0800

    Add files via upload

---
+++ SIAK_21A/src/id/ac/undiksha/siak/entities/Mahasiswa.java
@@ -0,0 +1,22 @@
+package id.ac.undiksha.siak.entities;
+
+public class Mahasiswa {
+	private String nama;
+	private String alamat;
+	private boolean jenisKelamin; //0 perempuan, 1 laki laki 
+	private String nim;
+	private String prodi;
+	private String jurusan;
+	private String fakultas;
+	
+	public String getNama() {
+	    this.nama = nama;
+	}
+	
+	public String getNama() {
+		return this.nama;
+	}
+
+
+
+}
